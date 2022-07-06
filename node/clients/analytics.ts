0c14dc609ed96f45b5065be05ed8500fe1ba57c1
Jean Fraga <jeandevjs@gmail.com>
2022-07-06 20:04:10 -0300

    feat: learn about services on vtexio

--- node/clients/analytics.ts
+++ node/clients/analytics.ts
@@ -1,3 +1,24 @@
-import { AppClient } from '@vtex/api'
+import { AppClient, InstanceOptions, IOContext } from '@vtex/api'
 
-export default class Analytics extends AppClient {}
+
+interface LiveUsersProduct {
+  slug: string; //id do produto
+  liveUsers: number; //quantidade de usuários visualizando o produto
+}
+
+
+export default class Analytics extends AppClient {
+
+  constructor ( context: IOContext, options?: InstanceOptions ) {
+    super (
+      'vtex.mocked-analytics@0.x', 
+      context, 
+      options
+      )
+  }
+
+  public getLiveUsers (): Promise<LiveUsersProduct[]> {
+    return this.http.get('_v/live-products')
+  }
+
+}
